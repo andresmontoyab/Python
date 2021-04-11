4e2d94b5fcb5f3573fdf093217033666e2faa049
andresmontoyab <and.montoya18@gmail.com>
2021-04-10 23:28:01 -0500

    Adding several views and logic to our flask app

--- python-projects/face-smash/app.py
+++ python-projects/face-smash/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, g
-from flask_login import LoginManager
+from flask import Flask, g, render_template, flash, url_for, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
 import models
+import forms
 
 DEBUG = True
 PORT = 8181
@@ -11,7 +13,7 @@ app.secret_key = 'sdhnlgndfjkgnsdSDFSDjfkgn324534@#!@$%fdlsgfsdSDFGS'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view('login')
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -20,23 +22,72 @@ def load_user(user_id):
     except models.DoesNotExist:
         return None
 
-
 @app.before_request
 def before_request():
     if not hasattr(g, 'db'):
-        g.db = models.DB
+        g.db = models.DATABASE
         g.db.connect()
+        g.user = current_user
 
 @app.after_request
 def after_request(response):
     g.db.close()
     return response
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Register finished ok')
+        models.User.create_user(
+            username = form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
 
-if __name__ == '__MAIN':
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash('Email or password is not correct')
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash('You are log in', 'success')
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=('GET', 'POST'))
+@login_required
+def logout():
+    logout_user()
+    flash('You are log out', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/new_post', methods=('GET', 'POST'))
+def post():
+    form = forms.PostForm()
+    if form.validate_on_submit():
+        models.Post.create(username= g.user._get_current_object(),
+                           content= form.content.data.strip())
+        flash("Post saved", 'success')
+        return redirect(url_for('index'))
+    return render_template('post.html', form= form)
+
+@app.route(('/'))
+def index():
+    stream = models.Post.select().limit(15)
+    return render_template('stream.html', stream=stream)
+
+if __name__ == '__main__':
     models.initialize()
     models.User.create_user(
-        name = 'Andres',
+        username = 'Andres',
         email = 'Andres@gmail.com',
         password = 'thepassword123'
     )
--- python-projects/face-smash/models.py
+++ python-projects/face-smash/models.py
@@ -5,15 +5,19 @@ from flask_bcrypt import generate_password_hash
 
 DATABASE = SqliteDatabase('social.db')
 
+
 class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=120)
-    joined_at = DateTimeField(default=datetime.datetime.now())
+    joined_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = DATABASE
-        order_by = ('joined_at')
+        order_by = ('-joined_at',)
+
+    def get_posts(self):
+        return Post.select().where(Post.user == self)
 
     @classmethod
     def create_user(cls, username, email, password):
@@ -23,9 +27,20 @@ class User(UserMixin, Model):
                 email=email,
                 password=generate_password_hash(password))
         except IntegrityError:
-            raise ValueError('User already exist')
+            pass
+            #raise ValueError('User already exist')
+
+
+class Post(Model):
+    username = ForeignKeyField(User, related_name='posts',)
+    time_stamp = DateTimeField(default=datetime.datetime.now)
+    content = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-joined_at',)
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User], safe=True)
+    DATABASE.create_tables([User, Post], safe=True)
     DATABASE.close()
